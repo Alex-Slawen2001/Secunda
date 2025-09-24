1ed5d078e0e90708ca1d4778a2b3b8aa15b3e9c5
Alex-Slawen2001 <a.shumikhin01@mail.ru>
2025-09-25 02:12:53 +0500

    внес значительные корректировки

--- secunda/app/Http/Kernel.php
+++ secunda/app/Http/Kernel.php
@@ -1,8 +1,6 @@
 <?php
 namespace App\Http;
-
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
 class Kernel extends HttpKernel
 {
     /**
@@ -12,7 +10,6 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -20,7 +17,6 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
-
     /**
      * The application's route middleware groups.
      *
@@ -35,21 +31,17 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
         'api' => [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
-
     /**
      * The application's route middleware.
-     *
      * These middleware may be assigned to groups or used individually.
      *
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
         'auth.apikey' => \App\Http\Middleware\ApiKeyMiddleware::class,
-
     ];
 }
--- secunda/app/Http/Middleware/ApiKeyMiddleware.php
+++ secunda/app/Http/Middleware/ApiKeyMiddleware.php
@@ -1,12 +1,14 @@
 <?php
+
 namespace App\Http\Middleware;
 use Closure;
+use Illuminate\Http\Request;
 class ApiKeyMiddleware
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $key = $request->header('X-API-KEY');
-        if ($key !== config('app.api_key')) {
+        if ($key !== config('api.key')) {
             return response()->json(['error' => 'Invalid API Key'], 401);
         }
         return $next($request);
