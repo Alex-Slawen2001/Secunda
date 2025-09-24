4dd94c60dd68df9fb09c5ad6978f2507f3d95aa9
Alex-Slawen2001 <a.shumikhin01@mail.ru>
2025-09-24 23:47:27 +0500

    запил модели

---
+++ secunda/app/Models/OrganizationPhone.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+class OrganizationPhone extends Model
+{
+    protected $fillable = ['organization_id', 'phone'];
+    public function organization() { return $this->belongsTo(Organization::class); }
+}
