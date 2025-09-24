740259dd828d6a5c202ea5fbffd0c7ccd51b240b
Alex-Slawen2001 <a.shumikhin01@mail.ru>
2025-09-24 23:50:32 +0500

    Запилил роуты

--- secunda/routes/web.php
+++ secunda/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\BuildingController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth.apikey')->group(function() {
+    Route::get('/organizations/building/{building}', [OrganizationController::class, 'byBuilding']);
+    Route::get('/organizations/activity/{activity}', [OrganizationController::class, 'byActivity']);
+    Route::get('/organizations/nearby', [OrganizationController::class, 'byLocation']);
+    Route::get('/organizations/{id}', [OrganizationController::class, 'show']);
+    Route::get('/organizations/search/name', [OrganizationController::class, 'searchByName']);
+    Route::get('/organizations/search/activity', [OrganizationController::class, 'searchByActivity']);
+    Route::get('/buildings', [BuildingController::class, 'index']);
 });
